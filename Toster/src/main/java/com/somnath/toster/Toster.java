b5d3f975d47ff1673c630b8c598f7ae2c38184fc
somna <somnathnath8482@gmail.com>
2021-10-19 21:36:01 +0530

    okk

--- Toster/src/main/java/com/somnath/toster/Toster.java
+++ Toster/src/main/java/com/somnath/toster/Toster.java
@@ -1,2 +1,12 @@
-package com.somnath.toster;public class Toster {
+package com.somnath.toster;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Toster {
+                            public void prompt(String s, Context context){
+                                Toast.makeText(context, s, Toast.LENGTH_LONG).show();
+
+                            }
+
 }
